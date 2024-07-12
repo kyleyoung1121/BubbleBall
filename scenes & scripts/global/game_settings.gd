1bc7ee44e3ff3a4e1546e53b59a2a9261eca8d0c
Kyle Young <kyleyoung1121@gmail.com>
2024-07-12 02:58:46 -0400

    Added map options & auto-reset

--- scenes & scripts/global/game_settings.gd
+++ scenes & scripts/global/game_settings.gd
@@ -1,9 +1,12 @@
 extends Node
 
-
+# General
+var points_to_win = 3
 var game_time_scale = 0.85
 var slow_mo_scale = 0.45
 
+# Players
 var player_mass = 0.4
 
-var bubble_size = 1.7
+# Bubbles
+var bubble_size = 1.4
